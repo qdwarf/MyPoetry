b8631a8287e80139380b53f0b33e071914f9673d
qdwarf <93176779+qdwarf@users.noreply.github.com>
2021-10-30 02:01:46 +0900

    Add to sketch
    p5.js에서 다운로드 받은 sketch.js를 추가한다.

---
+++ Poetic_2021_10_29_16_35_02/sketch.js
@@ -0,0 +1,72 @@
+let impression = [];
+
+function setup() {
+  createCanvas(600, 600);
+
+}
+
+function mousePressed() {
+  let p = new Impression(mouseX, mouseY, random(2,4));
+  impression.push(p);
+}
+
+function draw() {
+  background(50,30);
+
+  fill(255);
+  noStroke();
+  if(mouseIsPressed){
+    ellipse (width/2,height/2,400);
+  } else {
+    ellipse (width/2,height/2,300);
+  }
+  for(let i = 0; i < impression.length; i++){
+    let fall = createVector(0, 0.2*impression[i].mass);
+    impression[i].applyForce(fall);
+    
+  impression[i].update();
+  impression[i].display();
+  impression[i].edges();
+    
+  }
+}
+  
+function Impression(x, y, m) {
+  this.pos = createVector(x, y);
+  this.vel = createVector(0, 0);
+  this.acc = createVector(0, 0);
+  this.mass = m;
+  
+  this.applyForce = function(force) {
+    let f = force.copy();
+    f.div(this.mass);
+    this.acc.add(f);
+  }
+  
+  this.update = function() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    
+    this.acc.set(0,0);
+  }
+  
+  this.display = function() {
+    fill(random(0,255),random(0,255),random(0,255),90);
+    ellipse(this.pos.x, this.pos.y, this.mass*10, this.mass*10);
+
+
+  }
+  
+  this.edges = function() {
+    if (this.pos.y > height) {
+      this.vel.y *= -0.5;
+      this.pos.y = height;
+    }
+    
+    if (this.pos.x > width) {
+      this.vel.x *= -0.1;
+      this.pos.x = width;
+    }    
+  }
+}
+  
